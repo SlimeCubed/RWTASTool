25c209d9cd2982d7de5f25eaba8b7e1deaff92ca
SlimeCubed <cubedslime@gmail.com>
2021-01-17 11:41:37 -0800

    Add project files.

---
+++ RWTASTool/DeterministicRNG.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
+using Random = UnityEngine.Random;
+using SRandom = System.Random;
+
+namespace RWTASTool
+{
+    public class DeterministicRNG : IDisposable
+    {
+        public static SRandom nonCriticalRNG = new SRandom();
+        //public static SRandom backupRng = new SRandom();
+
+        // GrafUpdate should be called a predictable number of times per frame
+
+        private static NativeDetour[] _detours;
+        private static bool _detoursOn;
+        
+        public DeterministicRNG(RainWorldGame game, int initialSeed)
+        {
+            On.RainWorldGame.RawUpdate += RainWorldGame_RawUpdate;
+            
+            nonCriticalRNG = new SRandom(initialSeed + int.MaxValue);
+
+            if(_detours != null)
+                ToggleDetours(false);
+
+            Random.seed = initialSeed;
+
+            if (_detours == null)
+            {
+                BindingFlags bf = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+                Type rc = typeof(Random);
+                Type dc = typeof(DeterministicRNG);
+                _detours = new NativeDetour[]
+                {
+                    new NativeDetour(rc.GetProperty("value", bf).GetGetMethod(), dc.GetMethod("Random_value", bf)),
+                    new NativeDetour(rc.GetProperty("seed", bf).GetGetMethod() , dc.GetProperty("Random_seed", bf).GetGetMethod()),
+                    new NativeDetour(rc.GetProperty("seed", bf).GetSetMethod() , dc.GetProperty("Random_seed", bf).GetSetMethod()),
+                    new NativeDetour(rc.GetMethod("RandomRangeInt", bf), dc.GetMethod("Random_RandomRangeInt", bf)),
+                    new NativeDetour(rc.GetMethod("RandomRange", bf, null, new Type[] { typeof(float), typeof(float) }, null), dc.GetMethod("Random_RangeFloat"))
+                };
+                _detoursOn = true;
+            }
+            ToggleDetours(false);
+        }
+
+        // Change the game's behavior when lagging
+        // Instead of performing the same number of physics updates, it just slows down physics
+        // This ensures a predictable amount of graphics updates per frame
+        private float _savedTimeStacker = -1f;
+        private FieldInfo _MainLoopProcess_myTimeStacker = typeof(MainLoopProcess).GetField("myTimeStacker", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        private void RainWorldGame_RawUpdate(On.RainWorldGame.orig_RawUpdate orig, RainWorldGame self, float dt)
+        {
+            // Do not allow RNG to advance when paused
+            ToggleDetours(self.pauseMenu != null);
+
+            // It is also important that the timeStacker remains unmodified
+            if(self.pauseMenu == null)
+            {
+                if (_savedTimeStacker == -1f)
+                    _savedTimeStacker = (float)_MainLoopProcess_myTimeStacker.GetValue(self);
+            } else
+            {
+                if (_savedTimeStacker != -1f)
+                    _MainLoopProcess_myTimeStacker.SetValue(self, _savedTimeStacker);
+            }
+
+            orig(self, 1f / Application.targetFrameRate);
+        }
+
+        public static float Random_RangeFloat(float min, float max)
+        {
+            return Random_value() * (max - min) + min;
+        }
+
+        public static int Random_RandomRangeInt(int min, int max)
+        {
+            return nonCriticalRNG.Next(min, max);
+        }
+
+        public static int Random_seed
+        {
+            get => nonCriticalRNG.Next();
+            set => nonCriticalRNG = new SRandom(value);
+        }
+
+        public static float Random_value()
+        {
+            return (float)nonCriticalRNG.NextDouble();
+        }
+
+        private void ToggleDetours(bool on)
+        {
+            if (on == _detoursOn) return;
+            if (on)
+            {
+                _detoursOn = true;
+                for (int i = 0; i < _detours.Length; i++)
+                    _detours[i].Apply();
+            } else
+            {
+                _detoursOn = false;
+                for (int i = 0; i < _detours.Length; i++)
+                    _detours[i].Undo();
+            }
+        }
+
+        public void Dispose()
+        {
+            On.RainWorldGame.RawUpdate -= RainWorldGame_RawUpdate;
+            ToggleDetours(false);
+        }
+    }
+}
---
+++ RWTASTool/TasIPC.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Pipes;
+using UnityEngine;
+using System.Threading;
+
+namespace RWTASTool
+{
+    public class TasIPC : IDisposable
+    {
+        public static string pipeName = "RWTasTool";
+        private Thread _ipcThread;
+
+        public enum ServerboundCode : byte
+        {
+            RequestInputs,
+            SetInputs
+        }
+
+        // Currently unused, since the mod will reply immediately when possible
+        public enum ClientboundCode : byte
+        {
+        }
+
+        public TasIPC()
+        {
+            _ipcThread = new Thread(ServerThread);
+        }
+        
+        public void ServerThread()
+        {
+            using (NamedPipeServerStream pipe = new NamedPipeServerStream(pipeName))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        pipe.WaitForConnection();
+                        Log("Client connected");
+
+                        // Handshake!
+                        // The mod writes first, if the external editor can handle this version then it can send this string back
+                        WriteString(pipe, TasMod.version);
+                        string ipcVersion = ReadString(pipe);
+
+                        if (ipcVersion != TasMod.version) throw new FormatException("External editor protocol has a mismatched version.");
+
+                        int code = pipe.ReadByte();
+                        if (code >= 0)
+                        {
+                            switch ((ServerboundCode)code)
+                            {
+                                case ServerboundCode.RequestInputs:
+                                    ClientGetInputs(pipe);
+                                    break;
+                                case ServerboundCode.SetInputs:
+                                    ClientSetInputs(pipe);
+                                    break;
+                                default:
+                                    throw new InvalidOperationException("Invalid packet code (" + code + ") sent to server.");
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(new Exception("Failed", e));
+                    }
+
+                    Thread.Sleep(1);
+                }
+            }
+        }
+
+        public void ClientGetInputs(NamedPipeServerStream pipe)
+        {
+            TasUI.InputFrame f = new TasUI.InputFrame();
+            TasUI.queueLock.EnterReadLock();
+            try
+            {
+                pipe.Write(BitConverter.GetBytes(TasUI.main.inputQueue.Count), 0, 4);
+                for (int readIndex = 0; readIndex < TasUI.main.inputQueue.Count; readIndex++)
+                {
+                    f.Load(TasUI.main.inputQueue[readIndex]);
+                    f.ToStream(pipe);
+                }
+            }
+            finally
+            {
+                TasUI.queueLock.ExitReadLock();
+            }
+        }
+
+        public void ClientSetInputs(NamedPipeServerStream pipe)
+        {
+            TasUI.queueLock.EnterWriteLock();
+            try
+            {
+                TasUI.main.inputQueue.Clear();
+                TasUI.main.queueIndex = 0;
+                TasUI.main.repeatIndex = 0;
+                TasUI.InputFrame f = new TasUI.InputFrame();
+                List<TasUI.TasInputPackage> inputBuffer = new List<TasUI.TasInputPackage>();
+                byte[] buffer = new byte[4];
+                pipe.Read(buffer, 0, 4);
+                int len = BitConverter.ToInt32(buffer, 0);
+                for (int i = 0; i < len; i++)
+                    if (f.FromStream(pipe))
+                        inputBuffer.Add(f.ToInputs());
+                    else
+                        throw new EndOfStreamException("End of stream encountered where input sequence was expected.");
+                TasUI.main.inputQueue.AddRange(inputBuffer);
+                TasUI.main.UpdateFrameLabels();
+            } finally
+            {
+                TasUI.queueLock.ExitWriteLock();
+            }
+        }
+        
+        private string ReadString(NamedPipeServerStream pipe)
+        {
+            int len = pipe.ReadByte();
+            if (len == -1) throw new EndOfStreamException("End of stream encountered where string data was expected.");
+            byte[] buffer = new byte[len];
+            if (pipe.Read(buffer, 0, len) != len) throw new EndOfStreamException("End of stream encountered where string data was expected.");
+            return Encoding.UTF8.GetString(buffer, 0, len);
+        }
+
+        private int WriteString(NamedPipeServerStream pipe, string str)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            if (buffer.Length >= 255) throw new ArgumentException("String data must be less than 256 bytes in length.");
+            pipe.WriteByte((byte)buffer.Length);
+            pipe.Write(buffer, 0, buffer.Length);
+            return buffer.Length;
+        }
+
+        public static void Log(string msg)
+        {
+            Debug.Log("[RWTAS IPC] " + msg);
+        }
+
+        public void Dispose()
+        {
+            _ipcThread.Abort();
+        }
+    }
+}
